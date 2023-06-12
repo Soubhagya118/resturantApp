a92c8681023e4ebffa3b78120e00a4952c625423
Soubhagya118 <rinkisoubhagyasamal118@gmail.com>
2023-06-12 08:36:14 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import { useState } from 'react';
+
+
 function App() {
+const [CartShown, setCartShown]= useState(false);
+  const showHandler =()=>{
+    setCartShown(true);
+  };
+
+  const hideHandler =()=>{
+    setCartShown(false);
+  }
+
+
   return (
     <>
-    <Cart/>
-     <Header/>
+    {CartShown && <Cart onClose ={hideHandler}/>}
+     <Header onShowCart={showHandler} />
      <main>
       <Meals />
      </main>
